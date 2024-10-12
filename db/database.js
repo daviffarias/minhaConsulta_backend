c03210a89232b3828741c0b032e37ca7e1265b07
daviffarias <daviffarias12@hotmail.com>
2024-10-12 16:33:06 -0300

    Update

--- db/database.js
+++ db/database.js
@@ -27,5 +27,16 @@ db.serialize(() => {
     FOREIGN KEY (userId) REFERENCES users(id)
   )`);
 });
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS consultations (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    userId INTEGER NOT NULL,
+    date TEXT NOT NULL,
+    doctor TEXT NOT NULL,
+    specialty TEXT NOT NULL,
+    status TEXT NOT NULL,
+    FOREIGN KEY (userId) REFERENCES users(id)
+  )`);
+});
 
 module.exports = db;
